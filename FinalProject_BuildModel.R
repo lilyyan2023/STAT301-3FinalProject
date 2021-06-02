ada02e5674a8221f5716bffb6112ed9f85c17a46
ckang2023 <cindykang2023@u.northwestern.edu>
2021-06-02 14:02:45 -0500

    save setup

--- FinalProject_BuildModel.R
+++ FinalProject_BuildModel.R
@@ -1,17 +1,23 @@
 # Load Packages and set seed
 library(tidyverse)
 library(tidymodels)
+library(themis)
 set.seed(123)
 # load data
 stroke_data <- readRDS("data/processed/stroke_data.rds")
 
-# Split data
+# Split data ----
 stroke_split <- initial_split(stroke_data, prop = 0.7, strata = stroke)
 stroke_train <- training(stroke_split)
 stroke_test <- testing(stroke_split)
-# Build recipe
-# no residence_type possibly
-stroke_recipe <- recipe(stroke ~ ., data = stroke_train) %>% 
+
+## Cross-Validation ----
+stroke_folds <- 
+  vfold_cv(data = stroke_train, v = 5, repeats = 3, strata = stroke)
+
+# Build recipe ----
+# original recipe - no over-sampling
+stroke_recipe_orig <- recipe(stroke ~ ., data = stroke_train) %>% 
   step_impute_bag(bmi) %>% # bag impute bmi missing values
   step_log(avg_glucose_level, bmi) %>% 
   step_interact(bmi ~ age) %>% 
@@ -19,31 +25,41 @@ stroke_recipe <- recipe(stroke ~ ., data = stroke_train) %>%
   step_zv(all_predictors()) %>% 
   step_normalize(all_predictors())
 
-prep(stroke_recipe) %>% 
+# recipe with 0.2 ratio over-sampling
+stroke_recipe_over_sampling_1 <- recipe(stroke ~ ., data = stroke_train) %>% 
+  # over-sampling, 0.2 ratio
+  step_upsample(stroke, over_ratio = 0.2) %>%
+  step_impute_bag(bmi) %>% # bag impute bmi missing values
+  step_log(avg_glucose_level, bmi) %>% 
+  step_interact(bmi ~ age) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_zv(all_predictors()) %>% 
+  step_normalize(all_predictors())
+
+# recipe with 0.5 ratio over-sampling
+stroke_recipe_over_sampling_2 <- recipe(stroke ~ ., data = stroke_train) %>% 
+  # over-sampling, 0.5 ratio
+  step_upsample(stroke, over_ratio = 0.5) %>%
+  step_impute_bag(bmi) %>% # bag impute bmi missing values
+  step_log(avg_glucose_level, bmi) %>% 
+  step_interact(bmi ~ age) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_zv(all_predictors()) %>% 
+  step_normalize(all_predictors())
+
+prep(stroke_recipe_orig) %>% 
   bake(new_data = NULL)
 
-## random forest model
-# Define model
-rf_model <- rand_forest(
-  mode = "classification",
-  mtry = tune(),
-  min_n = tune()
-) %>% 
-  set_engine("ranger", importance = "impurity")
+prep(stroke_recipe_over_sampling_1) %>% 
+  bake(new_data = NULL)
 
-# Set up tuning grid
-rf_params <- parameters(rf_model) %>% 
-  update(mtry = mtry(range = c(1, 20)))
+prep(stroke_recipe_over_sampling_2) %>% 
+  bake(new_data = NULL)
 
-# Define tuning grid
-rf_grid <- grid_regular(rf_params, levels = 5)
+# save setup ----
+save(stroke_recipe_orig, stroke_train, stroke_test, stroke_folds, 
+     stroke_data, file = "data/stroke_setup.rda")
 
-# Workflow
-rf_workflow <- workflow() %>% 
-  add_model(rf_model) %>% 
-  add_recipe(stroke_recipe)
-
-# en, knn, svm
 
 
 
